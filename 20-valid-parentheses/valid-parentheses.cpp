6a42ca489ead808cfce4ce9eb8866f364556aa5e
ssnehatiwari21 <sneha.24bce11287@vitbhopal.ac.in>
2026-07-13 11:27:59 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (38.11%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n=s.length();
+        stack<char> st;
+        for(int i=0;i<n;i++){
+            if(s[i]=='(' || s[i]=='{'|| s[i]=='['){
+                st.push(s[i]);
+            }else{
+                if(st.empty()){
+                    return false;
+                }
+                if((s[i]==')' && st.top()!='(')||(s[i]=='}' && st.top()!='{')||(s[i]==']' && st.top()!='[')){
+                   return false;
+                }
+                st.pop();
+            }
+        }
+        if(!st.empty()) return false;
+        return true;
+    }
+};
