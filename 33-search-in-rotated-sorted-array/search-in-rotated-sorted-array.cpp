fb496ecb077a18d1bb69c40d4c24d4f8147b4e58
ssnehatiwari21 <sneha.24bce11287@vitbhopal.ac.in>
2026-07-25 10:45:59 +0530

    Time: 2 ms (2.17%) | Memory: 15.1 MB (68.70%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,45 +4,23 @@ public:
         int n=nums.size();
         int low=0;
         int high=n-1;
-        int ans=-1;
-        int min=-1;
-        //find the minimum element -O(logn)
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(nums[mid]>nums[n-1]){
-                low=mid+1;
+            if(nums[mid]==target) return mid;
+            if(nums[low]<=nums[mid]){
+                if(nums[mid]>target && nums[low]<=target){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
             }else{
-                min=mid;
-                high=mid-1;
+                if(nums[mid]<target && nums[high]>=target){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
             }
         }
-
-        //apply binary search to the right part-O(logn)
-        low=0;
-        high=min-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]>target){
-                high=mid-1;
-            }else if(nums[mid]<target){
-                low=mid+1;
-            }else{
-                return mid;
-            }
-        }
-        //apply binary seach to the left part-O(logn)
-        low=min;
-        high=n-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]>target){
-                high=mid-1;
-            }else if(nums[mid]<target){
-                low=mid+1;
-            }else{
-                return mid;
-            }
-        }
-        return ans;
+        return -1;
     }
 };
