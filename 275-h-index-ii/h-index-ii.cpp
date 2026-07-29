67599ad2d449118705f42e5ba186fe8cd92d4a02
ssnehatiwari21 <sneha.24bce11287@vitbhopal.ac.in>
2026-07-29 10:40:47 +0530

    Time: 3 ms (9.95%) | Memory: 22.6 MB (47.33%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool citedPapers(vector<int>& citations,int h){
+        int n=citations.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            if(citations[i]>=h){
+                count++;
+            }
+        }
+        return count>=h;
+    }
+    int hIndex(vector<int>& citations) {
+        int n=citations.size();
+        int low=1;
+        int high=*max_element(citations.begin(),citations.end());
+        int ans=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(citedPapers(citations,mid)){
+                ans=mid;
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+
+        }
+        return ans;
+    }
+};
