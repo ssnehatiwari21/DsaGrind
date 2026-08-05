2bd5451ba4593c49e3461d74e6b95c29818f7a8f
ssnehatiwari21 <sneha.24bce11287@vitbhopal.ac.in>
2026-08-05 11:13:34 +0530

    Time: 1 ms (11.10%) | Memory: 50.4 MB (5.36%) - LeetSync

--- 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
+++ 2047-find-a-peak-element-ii/find-a-peak-element-ii.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
-    vector<int> findPeakGrid(vector<vector<int>>& mat) {
-        int m=mat.size();
-        int n=mat[0].size();
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                int left=(j==0)?-1:mat[i][j-1];
-                int top=(i==0)?-1:mat[i-1][j];
-                int right=(j==n-1)?-1:mat[i][j+1];
-                int bottom=(i==m-1)?-1:mat[i+1][j];
-
-                if(mat[i][j]>left &&mat[i][j]>top && mat[i][j]>right && mat[i][j]>bottom) return {i,j};
+    int FindRowIndex(vector<vector<int>>& mat,int col,int r,int c){
+            int maxrow=0;
+            int maxValue=mat[0][col];
+            for(int i=1;i<r;i++){
+                if(mat[i][col]>maxValue){
+                    maxValue=mat[i][col];
+                    maxrow=i;
+                }
             }
+        return maxrow;
+    }
+    vector<int> findPeakGrid(vector<vector<int>>& mat) {
+        int row=mat.size();
+        int col=mat[0].size();
+        int low=0;
+        int high=col-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            int maxrowIndex=FindRowIndex(mat,mid,row,col);
+            int left=(mid==0)?-1:mat[maxrowIndex][mid-1];
+            int right=(mid==col-1)?-1:mat[maxrowIndex][mid+1];
+            if(mat[maxrowIndex][mid]>right && mat[maxrowIndex][mid]>left){
+                return {maxrowIndex,mid};
+            }
+            if(mat[maxrowIndex][mid]<left) high=mid-1;
+            else low=mid+1;
         }
         return {-1,-1};
     }
