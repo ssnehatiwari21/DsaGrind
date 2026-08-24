cb55c5f7993fae19cc22c71e4dd313161594b48f
ssnehatiwari21 <sneha.24bce11287@vitbhopal.ac.in>
2026-08-24 13:29:02 +0530

    Time: 165 ms (19.15%) | Memory: 98.7 MB (5.92%) - LeetSync

--- 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
+++ 2254-check-if-every-row-and-column-contains-all-numbers/check-if-every-row-and-column-contains-all-numbers.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     bool checkValid(vector<vector<int>>& matrix) {
+        //overall time complexity is O(n^2logn) and space complexity is O(n)
+
         int n=matrix[0].size();
         set<int> sr;
         set<int> sc;
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                sr.insert(matrix[i][j]);
+                sr.insert(matrix[i][j]); //logn
             }
-            if(sr.size()<n) return false;
-            sr.clear();
+            if(sr.size()!=n) return false; //log1
+            sr.clear();//log1
         }
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 sc.insert(matrix[j][i]);
             }
-            if(sc.size()<n) return false;
+            if(sc.size()!=n) return false;
             sc.clear();
         }
 
