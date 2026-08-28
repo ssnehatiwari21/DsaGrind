5d1a6facbfdedcc36be5fb87a2759a1e26441ebb
ssnehatiwari21 <sneha.24bce11287@vitbhopal.ac.in>
2026-08-28 12:24:36 +0530

    Time: 0 ms (100.00%) | Memory: 23.2 MB (20.57%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -15,18 +15,13 @@ public:
         if(node==nullptr) return 0;
         int l=height(node->left);
         int r=height(node->right);
+        if(l==-1 || r==-1) return -1;
+        if(abs(l-r)>1) return -1;
         return 1+max(l,r);
     }
     bool isBalanced(TreeNode* root) {
         if(root==nullptr) return true;
-        int leftHeight=height(root->left);
-        int rightHeight=height(root->right);
-
-        if(abs(leftHeight-rightHeight)>1) return false;
-
-        bool left=isBalanced(root->left);
-        bool right=isBalanced(root->right);
-        if(!left || !right) return false;
-        return true;
+        if(height(root)!=-1) return true;
+        return false;
     }
 };
