1b622e3bd9d6cd39fa5e0c987b20e8be240310db
ssnehatiwari21 <sneha.24bce11287@vitbhopal.ac.in>
2026-08-31 09:34:56 +0530

    Time: 10 ms (87.03%) | Memory: 37.5 MB (47.39%) - LeetSync

---
+++ 542-01-matrix/01-matrix.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int dir[4][2]={{1,0},{-1,0},{0,1},{0,-1}};
+    vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>> vis(m,vector<int>(n,0));
+        vector<vector<int>> ans(m,vector<int>(n));
+        int dis=0;
+        queue<pair<int,int>> q;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(vis[i][j]==0 && mat[i][j]==0){
+                    q.push({i,j});
+                    vis[i][j]=1;
+                    ans[i][j]=dis;
+                }
+            }
+        }
+
+        while(!q.empty()){
+            int size=q.size();
+            dis=dis+1;
+            for(int i=0;i<size;i++){
+                int tr=q.front().first;
+                int tc=q.front().second;
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int nr=tr+dir[i][0];
+                    int nc=tc+dir[i][1];
+
+                    if(nr>=0 && nc>=0 && nr<m && nc<n && vis[nr][nc]==0 && mat[nr][nc]==1){
+                        q.push({nr,nc});
+                        vis[nr][nc]=1;
+                        ans[nr][nc]=dis;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+};
