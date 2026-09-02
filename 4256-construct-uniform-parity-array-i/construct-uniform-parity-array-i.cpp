ff8cc3a983ef324eee1ef4bde789bc163ec05368
ssnehatiwari21 <sneha.24bce11287@vitbhopal.ac.in>
2026-09-02 22:19:00 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (36.43%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -1,7 +1,17 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        
+        //odd-odd=even
+        //even-even=even
+        //even-odd=odd
+
+        // if there is atleast one odd then it can make every even present odd
+        //if there is no odd then it is an array with only even values 
+        //hence always true
+
+
         return true;
+
+
     }
 };
